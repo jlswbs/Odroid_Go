ba26ac5424d40eda4b025789bcb801f660d1a023
jlswbs <jlswbs@users.noreply.github.com>
2022-03-21 16:32:23 +0100

    Add files via upload

--- Mira_Map/Mira_Map.ino
+++ Mira_Map/Mira_Map.ino
@@ -1,20 +1,18 @@
 // Mira chaotic map //
 
-#include "esp_partition.h"
-#include "esp_ota_ops.h"
-#include <M5Stack.h>
+#include <ESP32-Chimera-Core.h>
 
   #define SPEAKER 25
   #define WIDTH   320
   #define HEIGHT  240
   #define SCR    (WIDTH*HEIGHT)
 
+  #define ITER  2000
+
   uint32_t size = ((2*WIDTH) * (2*HEIGHT));
   uint16_t *col = NULL;
 
-  uint16_t coll;
-  uint16_t xx, yy;
-  int iterations = 10000;
+  uint16_t coll = TFT_WHITE;
   float x = 1.0f;
   float y = 0.0f;
   float f = 0.0f;
@@ -29,7 +27,7 @@ void rndrule(){
   x = 1.0f;
   y = 0.0f;
   f = 0.0f;
-  a = randomf(0.0f, 0.499f);
+  a = randomf(0.099f, 0.499f);
 
 }
 
@@ -43,7 +41,7 @@ void setup() {
   pinMode(SPEAKER, OUTPUT);
   digitalWrite(SPEAKER, LOW);
   M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-  M5.lcd.fillScreen(BLACK);
+  M5.Lcd.fillScreen(TFT_BLACK);
 
   rndrule();
 
@@ -53,16 +51,10 @@ void setup() {
 void loop() {
 
   if (M5.BtnA.wasPressed()) { rndrule(); M5.Lcd.drawString("RND", 10, 10, 2); }
-  if (M5.BtnC.wasPressed()) {
-    const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_ANY, NULL);
-    esp_ota_set_boot_partition(partition);
-    esp_restart();
-  }
-
-  coll = esp_random();
-
-  for (int i = 0; i < iterations; i++) {
-
+  if (M5.BtnC.wasPressed()) esp_restart();
+ 
+  for (int i = 0; i < ITER; i++){
+    
     float nx = x;
     float ny = y;
         
@@ -70,14 +62,20 @@ void loop() {
     f = a * x + ((2.0f*(1.0f-a)*powf(x,2.0f))/(1.0f+powf(x,2.0f)));
     y = -nx + f;
 
-    xx = (WIDTH/2) + 8.0f * x; 
-    yy = (HEIGHT/2) + 8.0f * y;
-    
+    uint16_t xx = (WIDTH/2) + 8.5f * x; 
+    uint16_t yy = (HEIGHT/2) + 8.5f * y; 
+      
+    if (coll == TFT_WHITE && xx == (WIDTH/2)+8){
+      memset((uint16_t *) col, 0, 4*SCR);
+      coll = TFT_WHITE;
+      a = randomf(0.099f, 0.499f);
+    } else if (xx == yy) coll = esp_random()%65535;
+      
     if(xx > 0 && xx < WIDTH && yy > 0 && yy < HEIGHT) col[xx+yy*WIDTH] = coll;
-
+    
   }
-
-  M5.lcd.pushRect(0, 0, WIDTH, HEIGHT,(uint16_t *) col);
+  
+  M5.Lcd.pushRect(0, 0, WIDTH, HEIGHT,(uint16_t *) col);
   M5.update();
 
 }
