bafc977a18a11a5f2c012449ec10a860b94a2c93
jlswbs <jlswbs@users.noreply.github.com>
2022-07-05 16:25:13 +0200

    Add files via upload

--- Biham_Middleton/Biham_Middleton.ino
+++ Biham_Middleton/Biham_Middleton.ino
@@ -1,15 +1,13 @@
-// Biham-Middleton-Levine Traffic Model //
+// Biham-Middleton-Levine traffic model //
 
-#include "esp_partition.h"
-#include "esp_ota_ops.h"
-#include <M5Stack.h>
+#include <ESP32-Chimera-Core.h>
 
   #define SPEAKER 25
   #define WIDTH   160
   #define HEIGHT  120
-  #define WFULL   320
-  #define HFULL   240
-  #define SCR     (WFULL*HFULL)
+  #define FULLW   320
+  #define FULLH   240
+  #define SCR     (FULLW*FULLH)
 
   uint32_t size = ((2*WIDTH) * (2*HEIGHT));
   
@@ -19,36 +17,39 @@
   int density = 35;
 
   int rule[] = {0,0,0,0,2,2,1,1,1,2,2,2,0,2,2,1,1,1,0,0,0,0,2,2,1,1,1};
-  uint8_t *pixles = NULL;
-  uint8_t *slexip = NULL;
+  uint8_t *world = NULL;
+  uint8_t *newworld = NULL;
   
 
-int color2state(int c){ return c == RED ? 1 : (c == GREEN ? 2 : 0); }
+int color2state(int c){ return c == TFT_RED ? 1 : (c == TFT_GREEN ? 2 : 0); }
 
-int state2color(int s){ return s == 1 ? RED : (s == 2 ? GREEN : WHITE); }
+int state2color(int s){ return s == 1 ? TFT_RED : (s == 2 ? TFT_GREEN : TFT_WHITE); }
 
-void rules() { for(int i=0; i<27; i++) rule[i] = esp_random()%3; }
+void rndrule() { for(int i=0; i<27; i++) rule[i] = esp_random()%3; }
 
-void trafficSet(){
+void rndseed(){
 
   memset((uint16_t *) col, 0, 4*SCR);
 
-  for(x=0; x<WIDTH; x++){
-    for(y=0; y<HEIGHT; y++){
+  density = 25 + esp_random()%50;
+
+  for(y=0; y<HEIGHT; y++){
+    
+    for(x=0; x<WIDTH; x++){
+    
       if((esp_random()%100)<density){
-        if((esp_random()%100)<50){
-          col[(2*x)+WFULL*(2*y)] = RED;
-        }else{
-          col[(2*x)+WFULL*(2*y)] = GREEN;
-        }
+        
+        if((esp_random()%100)<50) col[(2*x)+(2*y)*FULLW] = TFT_RED;
+        else col[(2*x)+(2*y)*FULLW] = TFT_GREEN;
+
       }
     }
   }
 
-  for(x=0; x<WIDTH; x++){
-    for(y=0; y<HEIGHT; y++){
-     pixles[y*WIDTH+x] = color2state(col[(2*x)+WFULL*(2*y)]);
-    }
+  for(y=0; y<HEIGHT; y++){
+    
+    for(x=0; x<WIDTH; x++) world[x+y*WIDTH] = color2state(col[(2*x)+(2*y)*FULLW]);
+
   }
 
 }
@@ -61,46 +62,41 @@ void setup() {
   pinMode(SPEAKER, OUTPUT);
   digitalWrite(SPEAKER, LOW);
   M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-  M5.lcd.fillScreen(BLACK);
+  M5.Lcd.fillScreen(TFT_BLACK);
 
-  pixles = (uint8_t*)ps_malloc(size);
-  slexip = (uint8_t*)ps_malloc(size);
+  world = (uint8_t*)ps_malloc(size);
+  newworld = (uint8_t*)ps_malloc(size);
   col = (uint16_t*)ps_malloc(4*SCR);
 
-  trafficSet();
+  rndseed();
   
 }
 
 
 void loop() {
 
-  if (M5.BtnA.wasPressed()) { rules(); trafficSet(); M5.Lcd.drawString("Rule", 10, 10, 2); }
-  if (M5.BtnB.wasPressed()) { trafficSet(); M5.Lcd.drawString("Seed", 10, 10, 2); }
-  if (M5.BtnC.wasPressed()) {
-    const esp_partition_t *partition = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_ANY, NULL);
-    esp_ota_set_boot_partition(partition);
-    esp_restart();
+  if (M5.BtnA.wasPressed()) { rndrule(); rndseed(); M5.Lcd.drawString("Rule", 10, 10, 2); }
+  if (M5.BtnB.wasPressed()) { rndseed(); M5.Lcd.drawString("Seed", 10, 10, 2); }
+  if (M5.BtnC.wasPressed()) esp_restart();
+
+  for(y = 0; y<HEIGHT; y++){
+    
+    for(x = 0; x<WIDTH; x++) newworld[x+y*WIDTH] = rule[world[y*WIDTH+(x+1)%WIDTH]+3*world[y*WIDTH+x]+9*world[y*WIDTH+(x+WIDTH-1)%WIDTH]];
+
   }
 
-  for(x = 0; x<WIDTH; x++){
-    for(y = 0; y<HEIGHT; y++){
-      slexip[y*WIDTH+x] = rule[pixles[y*WIDTH+(x+1)%WIDTH]+3*pixles[y*WIDTH+x]+9*pixles[y*WIDTH+(x+WIDTH-1)%WIDTH]];
+  for(y = 0; y<HEIGHT; y++){
+    
+    for(x = 0; x<WIDTH; x++){
+      
+      world[x+y*WIDTH] = rule[newworld[x+((y+1)%HEIGHT)*WIDTH]+3*newworld[y*WIDTH+x]+9*newworld[x+((y+HEIGHT-1)%HEIGHT)*WIDTH]];
+      col[(2*x)+(2*y)*FULLW] = state2color(world[x+y*WIDTH]);
+    
     }
-  }
-
-  for(x = 0; x<WIDTH; x++){
-    for(y = 0; y<HEIGHT; y++){
-      pixles[y*WIDTH+x] = rule[slexip[x+((y+1)%HEIGHT)*WIDTH]+3*slexip[y*WIDTH+x]+9*slexip[x+((y+HEIGHT-1)%HEIGHT)*WIDTH]];
-    }
+  
   }
   
-  for(x = 0; x<WIDTH; x++){
-    for(y = 0; y<HEIGHT; y++){
-      col[(2*x)+WFULL*(2*y)] = state2color(pixles[y*WIDTH+x]);
-    }
-  }
-  
-  M5.lcd.pushRect(0, 0, WFULL, HFULL,(uint16_t *) col);
+  M5.Lcd.pushRect(0, 0, FULLW, FULLH,(uint16_t *) col);
   M5.update();
 
 }
