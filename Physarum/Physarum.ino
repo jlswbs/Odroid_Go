af780f6be76f7fa90c690c3a7c8bea55b36b7e7c
jlswbs <jlswbs@users.noreply.github.com>
2022-08-25 19:53:50 +0200

    Add files via upload

--- Physarum/Physarum.ino
+++ Physarum/Physarum.ino
@@ -3,41 +3,37 @@
 #include <ESP32-Chimera-Core.h>
 
   #define SPEAKER 25
-  #define WIDTH   160
-  #define HEIGHT  120
-  #define FULLW   320
-  #define FULLH   240
-  #define SCR     (FULLW*FULLH)
+  #define WIDTH   320
+  #define HEIGHT  240
+  #define SCR     WIDTH*HEIGHT
 
   uint32_t size = ((2*WIDTH) * (2*HEIGHT));
   
+  #define ITER  50000
+  #define NUM   8
+
   uint16_t *col = NULL;
-
-  uint16_t RGB565( byte R, byte G, byte B){ return ( ((R & 0xF8) << 8) | ((G & 0xFC) << 3) | (B >> 3) );}
-
-  #define ITER  10000
-
-  uint16_t image;
   uint16_t *grid = NULL;
-  int state[8];
+  uint16_t coll[NUM];
+  uint16_t image;
   int t, q;
 
 void rndseed(){
 
   memset((uint16_t *) col, 0, 4*SCR);
 
-  for (int y = 0; y < HEIGHT; y++){
-    
+  for (int i = 0; i < NUM; i++) coll[i] = esp_random();
+
+  for (int y = 0; y < HEIGHT; y++){    
     for (int x = 0; x < WIDTH; x++){
       
       if(x == 0 || x == 1 || x == WIDTH-2 || x == WIDTH-1 || y == 0 || y == 1 || y == HEIGHT-2 || y == HEIGHT-1) grid[x+y*WIDTH] = 1;
       else grid[x+y*WIDTH] = 0;
 
     }
-
   }
   
-  for (int i = 1; i < 8; i++){
+  for (int i = 1; i < NUM; i++){
     
     int x = 2 * (5 + esp_random()%(WIDTH/2)-5);
     int y = 2 * (5 + esp_random()%(HEIGHT/2)-5);
@@ -45,23 +41,6 @@ void rndseed(){
 
   }
 
-}
-  
-void setup() {
-
-  srand(time(NULL));
-
-  M5.begin();
-  pinMode(SPEAKER, OUTPUT);
-  digitalWrite(SPEAKER, LOW);
-  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
-  M5.Lcd.fillScreen(TFT_BLACK);
-
-  grid = (uint16_t*)ps_malloc(size);
-  col = (uint16_t*)ps_malloc(4*SCR);
-
-  rndseed();
-  
 }
 
 void nextstep(){
@@ -105,10 +84,10 @@ void nextstep(){
     if(grid[x+y*WIDTH] >= 1000 && grid[x+y*WIDTH] < 2000){
       
       q = (grid[x+y*WIDTH]/100)-10;
-      if(state[q] == 0 && grid[(x+2)+y*WIDTH] == 0){ grid[(x+2)+y*WIDTH] = q*100; grid[(x+1)+y*WIDTH] = q*100; }
-      if(state[q] == 0 && grid[x+(y+2)*WIDTH] == 0){ grid[x+(y+2)*WIDTH] = q*100; grid[x+(y+1)*WIDTH] = q*100; }
-      if(state[q] == 0 && grid[(x-2)+y*WIDTH] == 0){ grid[(x-2)+y*WIDTH] = q*100; grid[(x-1)+y*WIDTH] = q*100; }
-      if(state[q] == 0 && grid[x+(y-2)*WIDTH] == 0){ grid[x+(y-2)*WIDTH] = q*100; grid[x+(y-1)*WIDTH] = q*100; }
+      if(grid[(x+2)+y*WIDTH] == 0){ grid[(x+2)+y*WIDTH] = q*100; grid[(x+1)+y*WIDTH] = q*100; }
+      if(grid[x+(y+2)*WIDTH] == 0){ grid[x+(y+2)*WIDTH] = q*100; grid[x+(y+1)*WIDTH] = q*100; }
+      if(grid[(x-2)+y*WIDTH] == 0){ grid[(x-2)+y*WIDTH] = q*100; grid[(x-1)+y*WIDTH] = q*100; }
+      if(grid[x+(y-2)*WIDTH] == 0){ grid[x+(y-2)*WIDTH] = q*100; grid[x+(y-1)*WIDTH] = q*100; }
     
     }
     
@@ -121,6 +100,23 @@ void nextstep(){
   }
   
 }
+  
+void setup() {
+
+  srand(time(NULL));
+
+  M5.begin();
+  pinMode(SPEAKER, OUTPUT);
+  digitalWrite(SPEAKER, LOW);
+  M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+  M5.Lcd.fillScreen(TFT_BLACK);
+
+  grid = (uint16_t*)ps_malloc(size);
+  col = (uint16_t*)ps_malloc(4*SCR);
+
+  rndseed();
+  
+}
 
 void loop() {
 
@@ -129,30 +125,18 @@ void loop() {
 
   nextstep();
 
-  memset((uint16_t *) col, 0, 4*SCR);
-
-  for (int y = 0; y < HEIGHT; y++){
+  for (int i = 0; i < SCR; i++){
     
-    for (int x = 0; x < WIDTH; x++){
-    
-      if(grid[x+y*WIDTH] == 1) image = RGB565(0,0,0);
-      if(grid[x+y*WIDTH] >= 100 && grid[x+y*WIDTH] < 1000){
-        q = grid[x+y*WIDTH]/100;
-        if(q == 1) image = RGB565(0,0,255);
-        if(q == 2) image = RGB565(0,255,0);
-        if(q == 3) image = RGB565(0,255,255);
-        if(q == 4) image = RGB565(255,0,0);
-        if(q == 5) image = RGB565(255,0,255);
-        if(q == 6) image = RGB565(255,255,0);
-        if(q == 7) image = RGB565(255,255,255);
+    if(grid[i] >= 100 && grid[i] < 1000){
+      q = (grid[i]/100)%NUM;
+      image = coll[q];    
+    } else image = TFT_BLACK;
       
-        col[(2*x)+(2*y)*FULLW] = image;
+    col[i] = image;
       
-      }
-    }
   }
   
-  M5.Lcd.pushRect(0, 0, FULLW, FULLH,(uint16_t *) col);
+  M5.Lcd.pushRect(0, 0, WIDTH, HEIGHT,(uint16_t *) col);
   M5.update();
 
 }
